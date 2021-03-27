474326f30ff9de642fee63d477bd3a381688e645
tomguitar0802 <78745783+tomguitar0802@users.noreply.github.com>
2021-03-27 17:56:27 +0900

    Update BDC.py

--- BDC.py
+++ BDC.py
@@ -8,8 +8,8 @@ Data=st.sidebar.file_uploader('Excelファイル')
 collision_energy=list(map(int,st.sidebar.text_input('CE').split()))
 focus_mz=list(map(int,st.sidebar.text_input('m/z').split()))
 st.sidebar.write("【オプション機能】")
-standardization=st.sidebar.checkbox('相対化',value=True)
-if standardization==True:
+standardization=st.sidebar.radio('数値処理選択',("相対強度","絶対強度"))
+if standardization=="相対強度":
     kyoudo="Relative Intensity(%)"
 else:
     kyoudo="Absolute Intensity"
@@ -19,7 +19,7 @@ y_label=st.sidebar.text_input('y軸ラベル',kyoudo)
 FontSize=st.sidebar.slider('フォントサイズ',5,25,15,1)
 yoko=st.sidebar.slider("グラフサイズ横",1.0,10.0,8.0,0.1)
 tate=st.sidebar.slider("グラフサイズ縦",1.0,10.0,6.0,0.1)
-hanrei=st.sidebar.checkbox('凡例グラフ内表示',value=True)
+hanrei=st.sidebar.radio('凡例表示位置',("グラフ内","グラフ外"))
 marker=st.sidebar.checkbox('マーカー',value=True)
 st.sidebar.write("【Excel読込設定】")
 skip_rows=int(st.sidebar.text_input('スキップ','7'))
@@ -36,7 +36,7 @@ if Data is not None:
 
     def read_sheet(a):
         df=pd.read_excel(Data,sheet_name=Sheet[a],skiprows=skip_rows)
-        if standardization==True:
+        if standardization=="相対強度":
             df=pd.DataFrame({'mz':round(df[mass]),'%':df[intensity]/sum(df[intensity])*100})
         else:
             df=pd.DataFrame({'mz':round(df[mass]),'%':df[intensity]})
@@ -63,9 +63,8 @@ ax.set_ylabel(y_label,size=FontSize)
 ax.set_xticks(collision_energy)
 ax.set_title(graph_title,size=FontSize+3)
 ax.ticklabel_format(useOffset=False,useMathText=True)
-if hanrei==True:
+if hanrei=="グラフ内":
     ax.legend()
 else:
     ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
 st.pyplot(fig)
-
