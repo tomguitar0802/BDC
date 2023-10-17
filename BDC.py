422982f71b5b84b11b01c52557d6aa86b413b1b8
tomguitar0802 <78745783+tomguitar0802@users.noreply.github.com>
2023-10-17 17:35:32 +0900

    Update BDC.py

--- BDC.py
+++ BDC.py
@@ -2,69 +2,55 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title('ブレイクダウンカーブ代行業者')
-st.sidebar.write("【必須項目】")
-Data=st.sidebar.file_uploader('Excelファイル')
-collision_energy=list(map(int,st.sidebar.text_input('CE').split()))
-focus_mz=list(map(int,st.sidebar.text_input('m/z').split()))
-st.sidebar.write("【オプション機能】")
-standardization=st.sidebar.radio('数値処理選択',("相対強度","絶対強度"))
-if standardization=="相対強度":
-    kyoudo="Relative Intensity(%)"
-else:
-    kyoudo="Absolute Intensity"
-graph_title=st.sidebar.text_input('グラフタイトル','')
-x_label=st.sidebar.text_input('x軸ラベル','Collision Energy(eV)')
-y_label=st.sidebar.text_input('y軸ラベル',kyoudo)
-FontSize=st.sidebar.slider('フォントサイズ',5,25,15,1)
-yoko=st.sidebar.slider("グラフサイズ横",1.0,10.0,8.0,0.1)
-tate=st.sidebar.slider("グラフサイズ縦",1.0,10.0,6.0,0.1)
-hanrei=st.sidebar.radio('凡例表示位置',("グラフ内","グラフ外"))
-marker=st.sidebar.checkbox('マーカー',value=True)
-st.sidebar.write("【Excel読込設定】")
-skip_rows=int(st.sidebar.text_input('スキップ','7'))
-mass=st.sidebar.text_input('m/z参照','Mass')
-intensity=st.sidebar.text_input('強度参照','Intensity')
+#-----------------------入力-----------------------
+Path="BDC.xlsx"                         #ファイル名
+mass_list=[100,200,300]                 #m/zリスト
+N_round=0                               #四捨五入の桁
+relative=True                           #相対or絶対
+legend_out=True                         #凡例の位置
+#--------------------------------------------------
 
-fig,ax=plt.subplots(figsize=(yoko,tate))
+Path=st.sidebar.file_uploader('Excelファイル')
+y_label="Relative Intensity (%)"
 
-DF,mz,Sheet=[],[],[]
-
-if Data is not None:
-    for i in range(len(collision_energy)):
-        Sheet.append("Sheet"+str(i+1))
-
-    def read_sheet(a):
-        df=pd.read_excel(Data,sheet_name=Sheet[a],skiprows=skip_rows)
-        if standardization=="相対強度":
-            df=pd.DataFrame({'mz':round(df[mass]),'%':df[intensity]/sum(df[intensity])*100})
+if Path is not None:
+    mass_list=list(map(int,st.sidebar.text_input('m/z').split(",")))
+    relative=st.sidebar.radio('強度',("相対","絶対"))
+    N=st.sidebar.slider('四捨五入の桁',0,5,0,1)
+    legend_out=st.sidebar.radio('凡例',("グラフ外","グラフ内"))
+    
+    #ExcelからDataFrameの作成
+    CE_list=pd.ExcelFile(Path).sheet_names
+    for CE in CE_list:
+        df=pd.read_excel(Path,sheet_name=CE,skiprows=7)
+        df=df.round({"Mass":N_round})
+        df=df.groupby("Mass").sum()
+        df=df.rename(columns={"Intensity":CE})
+        if CE==CE_list[0]:
+            DF=df
         else:
-            df=pd.DataFrame({'mz':round(df[mass]),'%':df[intensity]})
-        Df= df.loc[df.groupby('mz')['%'].idxmax()]
-        DF.append(Df.set_index('mz'))
+            DF=pd.concat([DF,df],axis=1)
 
-    for b in range(len(collision_energy)):
-        read_sheet(b)
+    #相対化処理と縦軸ラベルの設定
+    if relative=="相対":
+        DF=DF/DF.sum()
+        y_label="Relative Intensity (%)"
+    else:
+        y_label="Absolute Intensity (arb)"
 
-    for c in range(len(focus_mz)):
-        sublist=[]
-        for d in DF:
-            sublist.append(d.at[focus_mz[c],'%'])
-        mz.append(sublist)
+    #m/zリストを抜き出してプロット
+    fig,ax=plt.subplots()
+    ax.ticklabel_format(useOffset=False,useMathText=True)
+    for mass in mass_list:
+        plt.plot(CE_list,DF.loc[mass],
+                marker="o",
+                label="$\it{m/z}$"+str(mass))
 
-    for e in range(len(focus_mz)):
-        if marker==True:
-            ax.plot(collision_energy,mz[e],marker='o',label="m/z"+str(focus_mz[e]))
-        else:
-            ax.plot(collision_energy,mz[e],label="m/z"+str(focus_mz[e]))
-
-ax.set_xlabel(x_label,size=FontSize)
-ax.set_ylabel(y_label,size=FontSize)
-ax.set_xticks(collision_energy)
-ax.set_title(graph_title,size=FontSize+3)
-ax.ticklabel_format(useOffset=False,useMathText=True)
-if hanrei=="グラフ内":
-    ax.legend()
+#グラフの詳細設定
+plt.xlabel("Collision Energy (eV)")
+plt.ylabel(y_label)
+if legend_out=="グラフ外":
+    plt.legend(loc='upper left',bbox_to_anchor=(1,1))
 else:
-    ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
+    plt.legend()
 st.pyplot(fig)
