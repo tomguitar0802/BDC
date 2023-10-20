8205a3e8335138a7cd3ee6a12ad4e7177e0fb635
tomguitar0802 <78745783+tomguitar0802@users.noreply.github.com>
2023-10-20 10:28:38 +0900

    Update BDC.py

--- BDC.py
+++ BDC.py
@@ -2,23 +2,12 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#-----------------------入力-----------------------
-Path="BDC.xlsx"                         #ファイル名
-mass_list=[100,200,300]                 #m/zリスト
-N_round=0                               #四捨五入の桁
-relative=True                           #相対or絶対
-legend_out=True                         #凡例の位置
-#--------------------------------------------------
-
 Path=st.sidebar.file_uploader('Excelファイル')
-y_label="Relative Intensity (%)"
 
 if Path is not None:
-    mass_list=list(map(int,st.sidebar.text_input('m/z').split(",")))
-    relative=st.sidebar.radio('強度',("相対","絶対"))
-    N=st.sidebar.slider('四捨五入の桁',0,5,0,1)
-    legend_out=st.sidebar.radio('凡例',("グラフ外","グラフ内"))
-    
+    N_round=st.sidebar.slider('四捨五入の桁',0,5,0,1)
+    relative=st.sidebar.radio('縦軸',("絶対強度","%"))
+
     #ExcelからDataFrameの作成
     CE_list=pd.ExcelFile(Path).sheet_names
     for CE in CE_list:
@@ -31,26 +20,44 @@ if Path is not None:
         else:
             DF=pd.concat([DF,df],axis=1)
 
+    #m/zの選択
+    sugest=DF.sum(axis=1).sort_values(ascending=False).head(10).index.tolist()
+    if N_round==0:
+        sugest=map(int,sugest)
+        sugest=",".join(map(str,sugest))
+        mass_list=list(map(int,st.sidebar.text_input('m/z',sugest).split(",")))
+    else:
+        sugest=",".join(map(str,sugest))
+        mass_list=list(map(float,st.sidebar.text_input('m/z',sugest).split(",")))
+
+    #m/zリストの抜き出し
+    DF=DF.loc[mass_list,:]
+    
     #相対化処理と縦軸ラベルの設定
-    if relative=="相対":
+    if relative=="絶対強度":
+        y_label="Absolute Intensity (arb)"
+    else:
         DF=DF/DF.sum()
         y_label="Relative Intensity (%)"
-    else:
-        y_label="Absolute Intensity (arb)"
-
-    #m/zリストを抜き出してプロット
+    x_label=st.sidebar.text_input("x軸ラベル","Collision Energy (eV)")
+    
+    #プロットとグラフの詳細設定
     fig,ax=plt.subplots()
     ax.ticklabel_format(useOffset=False,useMathText=True)
+    marker=st.sidebar.radio("マーカー",("o",".",None))
     for mass in mass_list:
         plt.plot(CE_list,DF.loc[mass],
-                marker="o",
-                label="$\it{m/z}$"+str(mass))
+                marker=marker,
+                label="$\it{m/z}$ "+str(mass))
+    
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    legend_out=st.sidebar.radio('凡例',("グラフ外","グラフ内"))
+    if legend_out=="グラフ外":
+        plt.legend(loc='upper left',bbox_to_anchor=(1,1))
+    else:
+        plt.legend()
+    st.pyplot(fig)
 
-#グラフの詳細設定
-plt.xlabel("Collision Energy (eV)")
-plt.ylabel(y_label)
-if legend_out=="グラフ外":
-    plt.legend(loc='upper left',bbox_to_anchor=(1,1))
-else:
-    plt.legend()
-st.pyplot(fig)
+    #表の表示
+    st.write(DF)
