764b12edcde2a15cce753f282936b44f7b5609b2
tomguitar0802 <78745783+tomguitar0802@users.noreply.github.com>
2023-10-20 11:09:51 +0900

    Update BDC.py

--- BDC.py
+++ BDC.py
@@ -3,7 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 Path=st.sidebar.file_uploader('Excelファイル')
-
+ms=st.sidebar.radio('装置',("カンタム","オービ"))
+if ms=="カンタム":
+    skiprows=7
+else:
+    skiprows=8
 if Path is not None:
     N_round=st.sidebar.slider('四捨五入の桁',0,5,0,1)
     relative=st.sidebar.radio('縦軸',("絶対強度","%"))
@@ -11,7 +15,7 @@ if Path is not None:
     #ExcelからDataFrameの作成
     CE_list=pd.ExcelFile(Path).sheet_names
     for CE in CE_list:
-        df=pd.read_excel(Path,sheet_name=CE,skiprows=7)
+        df=pd.read_excel(Path,sheet_name=CE,skiprows=skiprows)
         df=df.round({"Mass":N_round})
         df=df.groupby("Mass").sum()
         df=df.rename(columns={"Intensity":CE})
